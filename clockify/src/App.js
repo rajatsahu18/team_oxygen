d688cd8f0c93789ba26ab41c2a7f418200e2dd31
Pooran8898 <poorancoc@gmail.com>
2021-04-14 21:57:46 +0530

    Login and Sign up Page Completed With Loading and Authentication

--- clockify/src/App.js
+++ clockify/src/App.js
@@ -1,13 +1,17 @@
 
 import './App.css';
 import { Page } from './Nilesh/Pages/Pages';
+import { Loader } from './Pooran/Pages/Loader/Loader';
+import { LoginPage } from './Pooran/Pages/Login/Login';
+import { SignupPage } from './Pooran/Pages/Signup/Signup';
 
 
 function App() {
   return (
     <div className="App">
-      <Page/>
-      
+      {/* <Page/> */}
+      <LoginPage/>
+      {/* <SignupPage/> */}
     </div>
   );
 }
