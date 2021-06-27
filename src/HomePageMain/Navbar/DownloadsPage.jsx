4ffa59d36ba4a8bb9917b427371352a0d50fb2cf
Rajat Sahu <sahurajat78@gmail.com>
2021-06-27 16:38:05 +0530

    update

--- src/HomePageMain/Navbar/DownloadsPage.jsx
+++ src/HomePageMain/Navbar/DownloadsPage.jsx
@@ -1,9 +1,5 @@
 import React from "react";
-<<<<<<< HEAD
 import { Download } from "../../Pages/DownloadPage/Download";
-=======
-import { Download } from "../../Rajat/Pages/DownloadPage/Download";
->>>>>>> b06b738ac1007872c71f642fdc4e87e216ac35c9
 
 function DownloadsPage() {
   return <div>
